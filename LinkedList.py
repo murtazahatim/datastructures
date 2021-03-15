017bfe364069ce3b93f53514bedeb6a0a9c0b0c7
Murtaza <dev@murtaza.info>
2021-03-15 23:23:22 +0530

    Added LinkedList.py Implementation

---
+++ LinkedList.py
@@ -0,0 +1,57 @@
+from Node import Node
+
+
+class LinkedList:
+    def __init__(self, arr=None):
+        if arr is None:
+            arr = []
+        self.head = Node(None, None)
+        self.tail = Node(None, self.head)
+        for elem in arr:
+            self.append(elem)
+
+    def append(self, elem):
+        last_node = self.tail.link
+        new_node = Node(elem)
+        last_node.set_link(new_node)
+        self.tail.set_link(new_node)
+
+    def push_front(self, elem):
+        if self.head.link is None:
+            self.append(elem)
+        else:
+            new_node = Node(elem, self.head.link)
+            self.head.set_link(new_node)
+
+    def delete_front(self):
+        if self.head.link is None:
+            raise Exception('Cannot delete from an empty linked list')
+
+        self.head.set_link(self.head.link.link)
+
+    def pop(self):
+        if self.head.link is None:
+            raise Exception('Cannot pop from an empty linked list')
+
+        previous_node = self.head
+        current_node = self.head.link
+
+        while current_node.link is not None:
+            previous_node = current_node
+            current_node = current_node.link
+
+        previous_node.set_link(None)
+        self.tail.set_link(previous_node)
+
+    def print(self):
+        arr = []
+        current_node = self.head
+
+        while True:
+            if current_node.data is not None:
+                arr.append(current_node.data)
+            if current_node.link is None:
+                break
+            current_node = current_node.link
+
+        print(arr)
---
+++ Node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, data=None, link=None):
+        self.data = data
+        self.link = link
+
+    def set_link(self, link):
+        self.link = link
+
+    def set_data(self, data):
+        self.data = data
